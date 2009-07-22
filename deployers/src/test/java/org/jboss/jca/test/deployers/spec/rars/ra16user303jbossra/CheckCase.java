7b22b393eac4e0ac62263b891f4743333664ef99
Jeff Zhang <jeff.zhang@ironjacamar.org>
2009-07-22 14:27:10 +0000

    [JBJCA-114] add test

---
+++ deployers/src/test/java/org/jboss/jca/test/deployers/spec/rars/ra16user303jbossra/CheckCase.java
@@ -0,0 +1,55 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.jca.test.deployers.spec.rars.ra16user303jbossra;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import javax.validation.Constraint;
+
+/**
+ * 
+ * @author <a href="mailto:jeff.zhang@jboss.org">Jeff Zhang</a>
+ * @version $Revision: $
+ */
+@Target({ METHOD, FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = CheckCaseValidator.class)
+@Documented
+public @interface CheckCase
+{
+   /**
+    * message
+    */
+   String message() default "{validator.checkcase}";
+   /**
+    * group
+    */
+   Class<?>[] groups() default { };
+   /**
+    * value
+    */
+   CaseMode value();
+}
---
+++ deployers/src/test/java/org/jboss/jca/test/deployers/spec/rars/ra16user303jbossra/CheckCaseValidator.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.jca.test.deployers.spec.rars.ra16user303jbossra;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * 
+ * @author <a href="mailto:jeff.zhang@jboss.org">Jeff Zhang</a>
+ * @version $Revision: $
+ */
+public class CheckCaseValidator implements ConstraintValidator<CheckCase, String> 
+{
+   private CaseMode caseMode;
+   
+   /**
+    * initialize
+    * @param constraintAnnotation CheckCase annotation
+    */
+   public void initialize(CheckCase constraintAnnotation)
+   {
+      this.caseMode = constraintAnnotation.value();
+   }
+   
+   /**
+    * isValid
+    * @param object string 
+    * @param constraintContext context 
+    * @return boolean valid or not
+    */
+   public boolean isValid(String object,
+         ConstraintValidatorContext constraintContext)
+   {
+      if (object == null)
+         return true;
+      if (caseMode == CaseMode.UPPER)
+         return object.equals(object.toUpperCase());
+      else
+         return object.equals(object.toLowerCase());
+   }
+
+}
