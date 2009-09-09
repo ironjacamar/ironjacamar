7204c778d77e460d0a0265418223a618fc446f77
Gurkan Erdogdu <gurkanerdogdu@yahoo.com>
2009-09-09 17:36:35 +0000

    Changing package name to lower case.

--- core/src/test/java/org/jboss/jca/test/core/connectionmanager/unit/nontx/NonTxConnectionManagerTestCase.java
+++ core/src/test/java/org/jboss/jca/test/core/connectionmanager/unit/nontx/NonTxConnectionManagerTestCase.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.jboss.jca.test.core.connectionmanager.unit.nonTx;
+package org.jboss.jca.test.core.connectionmanager.unit.nontx;
 
 import org.jboss.jca.core.api.ConnectionManager;
 
