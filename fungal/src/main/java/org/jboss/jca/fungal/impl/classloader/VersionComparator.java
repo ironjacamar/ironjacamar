e7fd1c9c834da6816f6b6d69bbc3710a2a92903a
Jesper Pedersen <jesper.pedersen@ironjacamar.org>
2010-04-06 10:57:16 +0000

    Speedup common check

--- fungal/src/main/java/org/jboss/jca/fungal/impl/classloader/VersionComparator.java
+++ fungal/src/main/java/org/jboss/jca/fungal/impl/classloader/VersionComparator.java
@@ -49,6 +49,12 @@ public class VersionComparator implements Comparator<String>, Serializable
     */
    public int compare(String o1, String o2)
    {
+      if (o1 == null && o2 == null)
+         return 0;
+
+      if (o1 != null && o2 != null && o1.equals(o2))
+         return 0;
+
       int oneMajor = 0;
       int oneMinor = 0;
       int onePatch = 0;
