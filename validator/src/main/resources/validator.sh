ba8c31a78bf16d2b8c6a011ccc61c6e9b9c6868e
Jesper Pedersen <jesper.pedersen@ironjacamar.org>
2010-07-27 13:30:54 +0000

    Use classpath directory in validator scripts

--- validator/src/main/resources/validator.sh
+++ validator/src/main/resources/validator.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-java -jar ironjacamar-validator-cli.jar $*
-
+java -classpath ironjacamar-validator-cli.jar:jbossxb.jar:jboss-logging-spi.jar:jboss-common-core.jar:jboss-metadata-rar.jar:jboss-metadata-common.jar:jboss-reflect.jar:jboss-mdr.jar:ironjacamar-spec-api.jar:papaki-core.jar:javassist.jar:ironjacamar-common-impl.jar:ironjacamar-validator.jar org.jboss.jca.validator.cli.Main $*
