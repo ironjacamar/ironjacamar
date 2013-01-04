d57f85a5e1ff40c66322b1786bca343c40ea4aae
Jesper Pedersen <jesper.pedersen@ironjacamar.org>
2013-01-04 13:51:58 +0000

    [JBJCA-957]  Don't pass property type to the recovery plugin

--- deployers/src/main/java/org/jboss/jca/deployers/common/AbstractResourceAdapterDeployer.java
+++ deployers/src/main/java/org/jboss/jca/deployers/common/AbstractResourceAdapterDeployer.java
@@ -1675,13 +1675,10 @@ public abstract class AbstractResourceAdapterDeployer
                                           {
                                              ConfigProperty c =
                                                 new ConfigPropertyImpl(null,
-                                                                       new XsdString(property.getKey(),
-                                                                                     null),
-                                                                       new XsdString("String",
-                                                                                     null),
-                                                                       new XsdString(property
-                                                                                     .getValue(),
-                                                                                     null), null);
+                                                                       new XsdString(property.getKey(), null),
+                                                                       XsdString.NULL_XSDSTRING,
+                                                                       new XsdString(property.getValue(), null),
+                                                                       null);
                                              configProperties.add(c);
                                           }
 
@@ -2223,11 +2220,11 @@ public abstract class AbstractResourceAdapterDeployer
                                                             new ConfigPropertyImpl(null,
                                                                                    new XsdString(property.getKey(),
                                                                                                  null),
-                                                                                   new XsdString("String",
-                                                                                                 null),
+                                                                                   XsdString.NULL_XSDSTRING,
                                                                                    new XsdString(property
                                                                                                  .getValue(),
-                                                                                                 null), null);
+                                                                                                 null),
+                                                                                   null);
                                                          configProperties.add(c);
                                                       }
 
