497948f114697f4bb789c85007b56b541d84a2c9
jesperpedersen <jesper.pedersen@ironjacamar.org>
2013-07-19 09:51:11 -0400

    A resource adapter which uses the statistics extension

---
+++ deployers/src/test/java/org/jboss/jca/deployers/test/rars/stat/StatisticsAdminObjectStatisticsPlugin.java
@@ -0,0 +1,115 @@
+/*
+ * IronJacamar, a Java EE Connector Architecture implementation
+ * Copyright 2013, Red Hat Inc, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.jca.deployers.test.rars.stat;
+
+import org.jboss.jca.core.spi.statistics.StatisticsPlugin;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ * Statistics plugin for the admin object
+ *
+ * @author <a href="jesper.pedersen@ironjacamar.org">Jesper Pedersen</a>
+ */
+public class StatisticsAdminObjectStatisticsPlugin implements StatisticsPlugin
+{
+   /** Serial version uid */
+   private static final long serialVersionUID = 1L;
+
+   private boolean enabled;
+   private Set<String> names;
+
+   /**
+    * Constructor
+    */
+   public StatisticsAdminObjectStatisticsPlugin()
+   {
+      this.enabled = true;
+      this.names = new HashSet<String>();
+      this.names.add("AdminObject");
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public Set<String> getNames()
+   {
+      return names;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public Class getType(String name)
+   {
+      return String.class;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public String getDescription(String name)
+   {
+      return getDescription(name, Locale.US);
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public String getDescription(String name, Locale locale)
+   {
+      return "AdminObject Description";
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public Object getValue(String name)
+   {
+      return "AdminObject Value";
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public boolean isEnabled()
+   {
+      return enabled;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public void setEnabled(boolean v)
+   {
+      enabled = v;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   public void clear()
+   {
+   }
+}
