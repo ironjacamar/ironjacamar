06884020019ceaf1b8d85c8b27ede231ba84a547
jesperpedersen <jesper.pedersen@ironjacamar.org>
2013-08-16 13:01:05 -0400

    Initial Gradle environment, and start on common/

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ IronJacamar
 IronJacamar implements the Java EE Connector Architecture 1.7 specification,
 and is certified in both the standalone profile, and full profile.
 
-IronJacamar is licensed under [GNU LESSER GENERAL PUBLIC LICENSE 2.1 (LGPL 2.1)](http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html "LGPL v2.1")
+IronJacamar is licensed under [Eclipse Public License v1.0 (EPL 1.0)](http://www.eclipse.org/legal/epl-v10.html "EPL v1.0")
 
 [http://www.ironjacamar.org/](http://www.ironjacamar.org/ "IronJacamar homepage")
 
@@ -14,7 +14,16 @@ Contributing
 * Write a blog about IronJacamar
 * Create a tutorial for IronJacamar
 * Help expand the [user guide](http://www.ironjacamar.org/doc/userguide/1.1/en-US/html/index.html)
-* Answer questions and share ideas on the [IRC channel](http://webchat.freenode.net/?channels=ironjacamar)
+* Help other users with their [questions](http://groups.google.com/forum/#!forum/ironjacamar-users)
+* Share ideas in the [developer forum](http://groups.google.com/forum/#!forum/ironjacamar-developers)
+* Chat with community members on the [IRC channel](http://webchat.freenode.net/?channels=ironjacamar)
 * Test [releases](http://www.ironjacamar.org/download.html)
 * Review [pull requests](http://github.com/ironjacamar/ironjacamar/pulls)
 * Start [hacking on IronJacamar](http://www.ironjacamar.org/doc/developerguide/1.1/en-US/html/ch02.html)
+
+Building
+--------
+
+### Bundle
+
+            gradle
---
+++ common/src/main/java/org/ironjacamar/common/CommonLogger.java
@@ -0,0 +1,70 @@
+/*
+ * IronJacamar, a Java EE Connector Architecture implementation
+ * Copyright 2013, Red Hat Inc, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the Eclipse Public License 1.0 as
+ * published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Eclipse
+ * Public License for more details.
+ *
+ * You should have received a copy of the Eclipse Public License 
+ * along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.ironjacamar.common;
+
+import org.jboss.logging.BasicLogger;
+import org.jboss.logging.annotations.Cause;
+import org.jboss.logging.annotations.LogMessage;
+import org.jboss.logging.annotations.Message;
+import org.jboss.logging.annotations.MessageLogger;
+
+import static org.jboss.logging.Logger.Level.ERROR;
+
+/**
+ * The common logger.
+ */
+@MessageLogger(projectCode = "IJ")
+public interface CommonLogger extends BasicLogger
+{
+   /**
+    * Parsing error for ra.xml
+    * @param url The url
+    * @param t The exception
+    */
+   @LogMessage(level = ERROR)
+   @Message(id = 210000, value = "Parsing error of ra.xml file: %s")
+   public void parsingErrorRaXml(String url, @Cause Throwable t);
+
+   /**
+    * Parsing error for ironjacamar.xml
+    * @param url The url
+    * @param t The exception
+    */
+   @LogMessage(level = ERROR)
+   @Message(id = 210001, value = "Parsing error of ironjacamar.xml file: %s")
+   public void parsingErrorIronJacamarXml(String url, @Cause Throwable t);
+
+   /**
+    * No connector
+    */
+   @LogMessage(level = ERROR)
+   @Message(id = 210002, value = "No @Connector was found and no definition in the ra.xml metadata either")
+   public void noConnector();
+
+   /**
+    * More than one connector
+    */
+   @LogMessage(level = ERROR)
+   @Message(id = 210003, value = "More than one @Connector was found but the correct one " +
+            "wasn't defined in the ra.xml metadata")
+   public void moreThanOneConnector();
+}
