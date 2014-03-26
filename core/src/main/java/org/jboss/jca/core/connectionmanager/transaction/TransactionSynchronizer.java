c5213d9ffbe8da13085bab01f38eca8dd844fbb8
jesperpedersen <jesper.pedersen@ironjacamar.org>
2014-03-26 10:51:08 -0400

    [JBJCA-1158] TransactionSynchronizer missing NPE guard

--- core/src/main/java/org/jboss/jca/core/connectionmanager/transaction/TransactionSynchronizer.java
+++ core/src/main/java/org/jboss/jca/core/connectionmanager/transaction/TransactionSynchronizer.java
@@ -161,6 +161,9 @@ public class TransactionSynchronizer implements Synchronization
     */
    public synchronized boolean removeEnlisted(Synchronization synch)
    {
+      if (enlisted == null)
+         return false;
+
       return enlisted.remove(synch);
    }
    
