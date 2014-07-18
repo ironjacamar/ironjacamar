cc482e0c0094e36fb3f1845b51ec6e5ecd5a7f23
jesperpedersen <jesper.pedersen@ironjacamar.org>
2014-07-18 09:13:03 -0400

    [JBJCA-1189] Revert XidWrapper change, but add pad guard

--- core/src/main/java/org/jboss/jca/core/tx/jbossts/XAResourceWrapperImpl.java
+++ core/src/main/java/org/jboss/jca/core/tx/jbossts/XAResourceWrapperImpl.java
@@ -22,6 +22,7 @@
 package org.jboss.jca.core.tx.jbossts;
 
 import org.jboss.jca.core.CoreLogger;
+import org.jboss.jca.core.spi.transaction.xa.XidWrapper;
 
 import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
@@ -100,6 +101,9 @@ public class XAResourceWrapperImpl implements org.jboss.jca.core.spi.transaction
     */
    public void commit(Xid xid, boolean onePhase) throws XAException
    {
+      if (pad)
+         xid = convertXid(xid);
+
       xaResource.commit(xid, onePhase);
    }
 
@@ -108,6 +112,9 @@ public class XAResourceWrapperImpl implements org.jboss.jca.core.spi.transaction
     */
    public void end(Xid xid, int flags) throws XAException
    {
+      if (pad)
+         xid = convertXid(xid);
+
       xaResource.end(xid, flags);
    }
 
@@ -116,6 +123,9 @@ public class XAResourceWrapperImpl implements org.jboss.jca.core.spi.transaction
     */
    public void forget(Xid xid) throws XAException
    {
+      if (pad)
+         xid = convertXid(xid);
+
       xaResource.forget(xid);
    }
 
@@ -161,6 +171,9 @@ public class XAResourceWrapperImpl implements org.jboss.jca.core.spi.transaction
     */
    public int prepare(Xid xid) throws XAException
    {
+      if (pad)
+         xid = convertXid(xid);
+
       return xaResource.prepare(xid);
    }
 
@@ -177,6 +190,9 @@ public class XAResourceWrapperImpl implements org.jboss.jca.core.spi.transaction
     */
    public void rollback(Xid xid) throws XAException
    {
+      if (pad)
+         xid = convertXid(xid);
+
       xaResource.rollback(xid);
    }
 
@@ -193,6 +209,9 @@ public class XAResourceWrapperImpl implements org.jboss.jca.core.spi.transaction
     */
    public void start(Xid xid, int flags) throws XAException
    {
+      if (pad)
+         xid = convertXid(xid);
+
       xaResource.start(xid, flags);
    }
 
@@ -229,6 +248,19 @@ public class XAResourceWrapperImpl implements org.jboss.jca.core.spi.transaction
    public String getJndiName()
    {
       return jndiName;
+   }
+
+   /**
+    * Return wrapper for given xid.
+    * @param xid xid
+    * @return return wrapper
+    */
+   private Xid convertXid(Xid xid)
+   {
+      if (xid instanceof XidWrapper)
+         return xid;
+      else
+         return new XidWrapperImpl(xid, pad, jndiName);
    }
    
    /**
