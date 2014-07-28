854c447d5b272fe858f0832b1b41c531025c8c7f
jesperpedersen <jesper.pedersen@ironjacamar.org>
2014-07-28 15:54:17 -0400

    [JBJCA-1199] Use AltKey as fallback

--- core/src/main/java/org/jboss/jca/core/connectionmanager/transaction/TransactionSynchronizer.java
+++ core/src/main/java/org/jboss/jca/core/connectionmanager/transaction/TransactionSynchronizer.java
@@ -388,20 +388,21 @@ public class TransactionSynchronizer implements Synchronization
       // Cleanup the maps
       if (records.remove(tx) == null)
       {
-         boolean found = false;
+         Object altKey = null;
          Iterator<Map.Entry<Transaction, Record>> iterator = records.entrySet().iterator();
-         while (!found && iterator.hasNext())
+         while (altKey == null && iterator.hasNext())
          {
             Map.Entry<Transaction, Record> next = iterator.next();
             if (next.getValue().getTransactionSynchronizer().equals(this))
             {
-               iterator.remove();
-               found = true;
+               altKey = next.getKey();
             }
          }
 
-         if (found)
+         if (altKey != null)
          {
+            records.remove(altKey);
+
             if (trace)
                log.tracef("Removed: %s [%s]", System.identityHashCode(tx), tx.toString());
          }
