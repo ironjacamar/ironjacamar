173c5571cd63d0297ee7a18272202ab6857a575e
vratsel <vrastseluev@gmail.com>
2014-12-25 15:06:48 +0300

    data sources tests update

--- testsuite/src/test/java/org/ironjacamar/common/metadata/ds/DataSources12TestCase.java
+++ testsuite/src/test/java/org/ironjacamar/common/metadata/ds/DataSources12TestCase.java
@@ -20,10 +20,28 @@
  */
 package org.ironjacamar.common.metadata.ds;
 
+import org.ironjacamar.common.api.metadata.common.Capacity;
+import org.ironjacamar.common.api.metadata.common.Credential;
+import org.ironjacamar.common.api.metadata.common.Extension;
+import org.ironjacamar.common.api.metadata.common.FlushStrategy;
+import org.ironjacamar.common.api.metadata.common.Recovery;
+import org.ironjacamar.common.api.metadata.ds.DataSource;
 import org.ironjacamar.common.api.metadata.ds.DataSources;
+import org.ironjacamar.common.api.metadata.ds.Driver;
+import org.ironjacamar.common.api.metadata.ds.DsPool;
+import org.ironjacamar.common.api.metadata.ds.DsSecurity;
+import org.ironjacamar.common.api.metadata.ds.DsXaPool;
+import org.ironjacamar.common.api.metadata.ds.Statement;
+import org.ironjacamar.common.api.metadata.ds.Statement.TrackStatementsEnum;
+import org.ironjacamar.common.api.metadata.ds.TimeOut;
+import org.ironjacamar.common.api.metadata.ds.TransactionIsolation;
+import org.ironjacamar.common.api.metadata.ds.Validation;
+import org.ironjacamar.common.api.metadata.ds.XaDataSource;
 
 import java.io.InputStream;
 import java.io.StringReader;
+import java.util.List;
+import java.util.Map;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamReader;
@@ -31,7 +49,9 @@ import javax.xml.stream.XMLStreamReader;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * DataSources 1.2 tests
@@ -56,6 +76,7 @@ public class DataSources12TestCase
       assertNotNull(ds);
 
       is.close();
+      checkDS(ds);
    }
 
    /**
@@ -79,7 +100,7 @@ public class DataSources12TestCase
       StringReader sr = new StringReader(ds.toString());
       XMLStreamReader xsr = XMLInputFactory.newInstance().createXMLStreamReader(sr);
       DataSources dsn = parser.parse(xsr);
-
+      checkDS(dsn);
       assertEquals(ds, dsn);
    }
 
@@ -111,5 +132,255 @@ public class DataSources12TestCase
       is2.close();
 
       assertEquals(ds1, ds2);
+   }
+   
+   /**
+    * Checks the data source parsed
+    * @param result of data source parsing
+    */
+   private void checkDS(DataSources ds) 
+   {
+      List<DataSource> listDs = ds.getDataSource();
+      assertEquals(1, listDs.size());
+      DataSource d = listDs.get(0);
+      
+      assertFalse(d.isJTA());
+      assertTrue(d.isSpy());
+      assertFalse(d.isEnabled());
+      assertFalse(d.isUseCcm());
+      assertFalse(d.isUseJavaContext());
+      assertEquals("java:jboss/datasources/complexDs", d.getJndiName());
+      assertEquals("complexDs_Pool", d.getPoolName());
+      
+      assertEquals("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", d.getConnectionUrl());
+      assertEquals("org.hsqldb.jdbcDriver", d.getDriverClass());
+      assertEquals("org.pg.JdbcDataSource", d.getDataSourceClass());
+      assertEquals("h2", d.getDriver());
+      Map<String, String> properties = d.getConnectionProperties();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("select 1", d.getNewConnectionSql());
+      assertEquals(":", d.getUrlDelimiter());
+      assertEquals("someClass", d.getUrlSelectorStrategyClassName());
+      assertEquals(TransactionIsolation.valueOf("2"), d.getTransactionIsolation());
+      
+      DsPool pool = d.getPool();
+      assertNotNull(pool);
+      assertEquals(1, (int)pool.getMinPoolSize());
+      assertEquals(2, (int)pool.getInitialPoolSize());
+      assertEquals(5, (int)pool.getMaxPoolSize());
+      assertTrue(pool.isPrefill());
+      assertTrue(pool.isUseStrictMin());
+      assertEquals(FlushStrategy.ALL_CONNECTIONS, pool.getFlushStrategy());
+      assertTrue(pool.isAllowMultipleUsers());
+      Capacity cp = pool.getCapacity();
+      assertNotNull(cp);
+      Extension e = cp.getIncrementer();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("ic", e.getClassName());
+      e = cp.getDecrementer();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("dc", e.getClassName());
+      
+      DsSecurity s = d.getSecurity();
+      assertNotNull(s);
+      assertEquals("sa", s.getUserName());
+      assertEquals("sa", s.getPassword());
+      e = s.getReauthPlugin();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass1", e.getClassName());
+      
+      Validation v = d.getValidation();
+      assertNotNull(v);
+      assertEquals("select 1", v.getCheckValidConnectionSql());
+      assertTrue(v.isBackgroundValidation());
+      assertTrue(v.isValidateOnMatch());
+      assertTrue(v.isUseFastFail());
+      assertEquals(2000L, (long)v.getBackgroundValidationMillis());
+      e = v.getValidConnectionChecker();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass2", e.getClassName());
+      e = v.getStaleConnectionChecker();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass3", e.getClassName());
+      e = v.getExceptionSorter();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass4", e.getClassName());
+      
+      TimeOut t = d.getTimeOut();
+      assertNotNull(t);
+      assertEquals(20000L, (long)t.getBlockingTimeoutMillis());
+      assertEquals(4L, (long)t.getIdleTimeoutMinutes());
+      assertEquals(120L, (long)t.getQueryTimeout());
+      assertEquals(100L, (long)t.getUseTryLock());
+      assertEquals(2L, (long)t.getAllocationRetry());
+      assertEquals(3000L, (long)t.getAllocationRetryWaitMillis());
+      assertTrue(t.isSetTxQueryTimeout());
+      
+      Statement st = d.getStatement();
+      assertNotNull(st);
+      assertEquals(30L, (long)st.getPreparedStatementsCacheSize());
+      assertTrue(st.isSharePreparedStatements());
+      assertEquals(TrackStatementsEnum.NOWARN, st.getTrackStatements());
+      
+      List<XaDataSource> xds = ds.getXaDataSource();
+      assertEquals(1, xds.size());      
+      XaDataSource xd = xds.get(0);
+      
+      assertFalse(xd.isSpy());
+      assertTrue(xd.isEnabled());
+      assertTrue(xd.isUseCcm());
+      assertTrue(xd.isUseJavaContext());
+      assertEquals("java:jboss/xa-datasources/complexXaDs", xd.getJndiName());
+      assertEquals("complexXaDs_Pool", xd.getPoolName());
+      
+      assertEquals("org.pg.JdbcXADataSource", xd.getXaDataSourceClass());
+      assertEquals("pg", xd.getDriver());
+      properties = xd.getXaDataSourceProperty();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("select 1", xd.getNewConnectionSql());
+      assertEquals(":", xd.getUrlDelimiter());
+      assertEquals("someClass", xd.getUrlSelectorStrategyClassName());
+      assertEquals(TransactionIsolation.TRANSACTION_READ_COMMITTED, xd.getTransactionIsolation());
+      
+      DsXaPool xpool = xd.getXaPool();
+      assertNotNull(xpool);
+      assertEquals(1, (int)xpool.getMinPoolSize());
+      assertEquals(2, (int)xpool.getInitialPoolSize());
+      assertEquals(5, (int)xpool.getMaxPoolSize());
+      assertTrue(xpool.isPrefill());
+      assertTrue(xpool.isUseStrictMin());
+      assertEquals(FlushStrategy.GRACEFULLY, xpool.getFlushStrategy());
+      assertTrue(xpool.isInterleaving());
+      assertTrue(xpool.isSameRmOverride());
+      assertTrue(xpool.isNoTxSeparatePool());
+      assertTrue(xpool.isPadXid());
+      assertFalse(xpool.isWrapXaResource());
+      assertTrue(xpool.isAllowMultipleUsers());
+      cp = xpool.getCapacity();
+      assertNotNull(cp);
+      e = cp.getIncrementer();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("ic", e.getClassName());
+      e = cp.getDecrementer();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("dc", e.getClassName());
+ 
+      s = xd.getSecurity();
+      assertNotNull(s);
+      assertEquals("HsqlDbRealm", s.getSecurityDomain());
+      e = s.getReauthPlugin();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass1", e.getClassName());
+      
+      Recovery r = xd.getRecovery();
+      assertNotNull(r);
+      assertFalse(r.getNoRecovery());
+      Credential c = r.getCredential();
+      assertNotNull(c);
+      assertEquals("HsqlDbRealm", c.getSecurityDomain());
+      e = r.getRecoverPlugin();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass5", e.getClassName());
+      
+      v = xd.getValidation();
+      assertNotNull(v);
+      assertEquals("select 1", v.getCheckValidConnectionSql());
+      assertTrue(v.isBackgroundValidation());
+      assertTrue(v.isValidateOnMatch());
+      assertTrue(v.isUseFastFail());
+      assertEquals(2000L, (long)v.getBackgroundValidationMillis());
+      e = v.getValidConnectionChecker();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass2", e.getClassName());
+      e = v.getStaleConnectionChecker();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass3", e.getClassName());
+      e = v.getExceptionSorter();
+      properties = e.getConfigPropertiesMap();
+      assertEquals(2, properties.size()); 
+      assertEquals("Property1", properties.get("name1"));
+      assertEquals("Property2", properties.get("name2"));
+      assertEquals("someClass4", e.getClassName());
+      
+      t = xd.getTimeOut();
+      assertNotNull(t);
+      assertEquals(20000L, (long)t.getBlockingTimeoutMillis());
+      assertEquals(4L, (long)t.getIdleTimeoutMinutes());
+      assertEquals(120L, (long)t.getQueryTimeout());
+      assertEquals(100L, (long)t.getUseTryLock());
+      assertEquals(2L, (long)t.getAllocationRetry());
+      assertEquals(3000L, (long)t.getAllocationRetryWaitMillis());
+      assertTrue(t.isSetTxQueryTimeout());
+      
+      st = xd.getStatement();
+      assertNotNull(st);
+      assertEquals(30L, (long)st.getPreparedStatementsCacheSize());
+      assertTrue(st.isSharePreparedStatements());
+      assertEquals(TrackStatementsEnum.TRUE, st.getTrackStatements());
+      
+      List<Driver> drivers = ds.getDrivers();
+      assertEquals(2, drivers.size());
+
+      Driver driver = ds.getDriver("h2");
+      assertTrue(drivers.contains(driver));
+      assertNotNull(driver);
+      assertEquals("h2", driver.getName());
+      assertEquals(null, driver.getMajorVersion());
+      assertEquals(null, driver.getMinorVersion());
+      assertEquals("com.h2database.h2", driver.getModule());
+      assertEquals(null, driver.getDriverClass());
+      assertEquals(null, driver.getXaDataSourceClass());
+      assertEquals("org.h2.jdbcx.JdbcDataSource", driver.getDataSourceClass());
+     
+      driver = ds.getDriver("pg");
+      assertNotNull(driver);
+      assertTrue(drivers.contains(driver));
+      assertEquals(9, (int)driver.getMajorVersion());
+      assertEquals(1, (int)driver.getMinorVersion());
+      assertEquals("org.pg.postgres", driver.getModule());
+      assertEquals("org.pg.Driver", driver.getDriverClass());
+      assertEquals("org.pg.JdbcDataSource", driver.getXaDataSourceClass());
+      assertEquals(null, driver.getDataSourceClass());
+
    }
 }
