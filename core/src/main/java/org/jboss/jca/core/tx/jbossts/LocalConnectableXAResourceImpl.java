ff62b8b23f59f9fbb9c15be40fef38efb872c436
Tom Jenkinson <tom.jenkinson@redhat.com>
2015-01-15 17:18:44 +0000

    BZ1181132 Updated to throw an RMFAIL if we have identified that the outcome of the commit is unknown at this time

--- core/src/main/java/org/jboss/jca/core/tx/jbossts/LocalConnectableXAResourceImpl.java
+++ core/src/main/java/org/jboss/jca/core/tx/jbossts/LocalConnectableXAResourceImpl.java
@@ -21,8 +21,19 @@
  */
 package org.jboss.jca.core.tx.jbossts;
 
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.resource.ResourceException;
+import javax.transaction.xa.XAException;
+import javax.transaction.xa.Xid;
+
+import org.jboss.jca.core.CoreBundle;
 import org.jboss.jca.core.spi.transaction.ConnectableResource;
 import org.jboss.jca.core.spi.transaction.ConnectableResourceListener;
+import org.jboss.jca.core.spi.transaction.local.LocalXAException;
+import org.jboss.logging.Messages;
 
 /**
  * Local connectable XA resource implementation.
@@ -31,12 +42,23 @@ import org.jboss.jca.core.spi.transaction.ConnectableResourceListener;
  */
 public class LocalConnectableXAResourceImpl extends LocalXAResourceImpl 
    implements ConnectableResource, org.jboss.tm.ConnectableResource
-{
+{    
+   /** The bundle */
+   private static CoreBundle bundle = Messages.getBundle(CoreBundle.class);
+    
    /** Connectable resource */
    private ConnectableResource cr;
    
    /** The connectable resource listener */
    private ConnectableResourceListener crl;
+   
+   // Oracle: 08000 (No more data to read from socket)
+   // DB2: 08001 (Insufficient data. ERRORCODE=-4499, SQLSTATE=08001)
+   // Sybase: JZ006 (Connection is already closed.)
+   // Postgres: 08006 (An I/O error occurred while sending to the backend.)
+   // Mysql: 08007 (Transaction resolution unknown.)
+   // Mssql: 08S01 (Server did not return a response. The connection has been closed.) 
+   private static final List<String> RMFAIL_SQL_ERRORCODES = Arrays.asList(new String[] {"08000", "08001", "JZ006", "08006", "08007", "08S01"});
 
    /**
     * Creates a new instance.
@@ -73,4 +95,36 @@ public class LocalConnectableXAResourceImpl extends LocalXAResourceImpl
    {
       this.crl = crl;
    }
+   
+   /**
+    * {@inheritDoc}
+    */
+   public void commit(Xid xid, boolean onePhase) throws XAException
+   {
+      if (!xid.equals(currentXid))
+      {
+         throw new LocalXAException(bundle.wrongXidInCommit(currentXid, xid), XAException.XAER_PROTO);
+         
+      }
+      
+      currentXid = null;
+
+      try
+      {
+         cl.getManagedConnection().getLocalTransaction().commit();
+      }
+      catch (ResourceException re)
+      {
+         connectionManager.returnManagedConnection(cl, true);
+         Throwable cause = re.getCause();
+         if (cause instanceof SQLException) {
+             SQLException sqle = (SQLException) cause;
+             String sqlState = sqle.getSQLState();
+             if (RMFAIL_SQL_ERRORCODES.contains(sqlState)) {
+                 throw new LocalXAException(bundle.couldNotCommitLocalTx(), XAException.XAER_RMFAIL, re);
+             }
+         }
+         throw new LocalXAException(bundle.couldNotCommitLocalTx(), XAException.XA_RBROLLBACK, re);
+      }
+   }   
 }
