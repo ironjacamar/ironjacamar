396bab5f84080a264eefe25de65712d80d3a918f
Tomas Hofman <thofman@redhat.com>
2017-02-07 10:55:03 +0100

    JBJCA-1338 Valid connection checker can unintentionally open a transaction

--- adapters/src/main/java/org/jboss/jca/adapters/jdbc/BaseWrapperManagedConnection.java
+++ adapters/src/main/java/org/jboss/jca/adapters/jdbc/BaseWrapperManagedConnection.java
@@ -345,6 +345,16 @@ public abstract class BaseWrapperManagedConnection implements ManagedConnection,
       synchronized (stateLock)
       {
          jdbcAutoCommit = true;
+         if (jdbcAutoCommit != underlyingAutoCommit)
+         {
+            try {
+               con.setAutoCommit(jdbcAutoCommit);
+               underlyingAutoCommit = jdbcAutoCommit;
+            } catch (SQLException e) {
+               mcf.log.errorResettingAutoCommit(mcf.getJndiName(), e);
+            }
+         }
+
          jdbcReadOnly = readOnly;
          if (jdbcTransactionIsolation != transactionIsolation)
          {
