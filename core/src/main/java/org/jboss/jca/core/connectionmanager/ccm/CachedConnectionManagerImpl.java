37986c51eea4e34b10476674df20c0facac77dbd
JiriOndrusek <jondruse@redhat.com>
2017-11-09 14:49:12 +0100

    [JBJCA-1358] - Container is not cleaning up container-managed JMSContext

--- core/src/main/java/org/jboss/jca/core/connectionmanager/ccm/CachedConnectionManagerImpl.java
+++ core/src/main/java/org/jboss/jca/core/connectionmanager/ccm/CachedConnectionManagerImpl.java
@@ -686,6 +686,14 @@ public class CachedConnectionManagerImpl implements CachedConnectionManager
        */
       public void beforeCompletion()
       {
+         // No-action
+      }
+
+      /**
+       * {@inheritDoc}
+       */
+      public void afterCompletion(int status)
+      {
          closing.set(true);
 
          for (Iterator<Object> i = connections.iterator(); i.hasNext();)
@@ -694,14 +702,6 @@ public class CachedConnectionManagerImpl implements CachedConnectionManager
          }
 
          connections.clear();
-      }
-
-      /**
-       * {@inheritDoc}
-       */
-      public void afterCompletion(int status)
-      {
-         // No-action
       }
    }
 
