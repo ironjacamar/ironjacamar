0cb4e1443add19aaa831ab6a1394aa241823d3e5
Tomasz Adamski <tadamski@redhat.com>
2021-01-20 15:01:15 +0100

    [JBJCA-1397] Datasource test: throw ResourceException with failure description instead of returning boolean

--- core/src/main/java/org/jboss/jca/core/connectionmanager/pool/strategy/PoolByCri.java
+++ core/src/main/java/org/jboss/jca/core/connectionmanager/pool/strategy/PoolByCri.java
@@ -1,6 +1,6 @@
 /*
  * IronJacamar, a Java EE Connector Architecture implementation
- * Copyright 2008-2009, Red Hat Inc, and individual contributors
+ * Copyright 2021, Red Hat Inc, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -71,17 +71,17 @@ public class PoolByCri extends AbstractPool
    /**
     * {@inheritDoc}
     */
-   public boolean testConnection()
+   public void testConnection() throws ResourceException
    {
-      return false;
+      throw new ResourceException();
    }
 
    /**
     * {@inheritDoc}
     */
-   public boolean testConnection(ConnectionRequestInfo cri, Subject subject)
+   public void testConnection(ConnectionRequestInfo cri, Subject subject) throws ResourceException
    {
-      return internalTestConnection(cri, null);
+      internalTestConnection(cri, null);
    }
 
    /**
