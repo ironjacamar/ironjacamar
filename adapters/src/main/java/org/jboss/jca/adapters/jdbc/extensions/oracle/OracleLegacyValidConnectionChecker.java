8630e6f4897e7736c59dae6c83103f2645abcc9f
Ivo Studensky <istudens@redhat.com>
2021-09-17 14:59:30 +0200

    [JBJCA-1421] OracleValidConnectionChecker now uses isValid method, the original impl has been preserved in OracleLegacyValidConnectionChecker

--- adapters/src/main/java/org/jboss/jca/adapters/jdbc/extensions/oracle/OracleLegacyValidConnectionChecker.java
+++ adapters/src/main/java/org/jboss/jca/adapters/jdbc/extensions/oracle/OracleLegacyValidConnectionChecker.java
@@ -1,6 +1,6 @@
 /*
  * IronJacamar, a Java EE Connector Architecture implementation
- * Copyright 2021, Red Hat Inc, and individual contributors
+ * Copyright 2010, Red Hat Inc, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -22,24 +22,57 @@
 
 package org.jboss.jca.adapters.jdbc.extensions.oracle;
 
-import org.jboss.jca.adapters.jdbc.extensions.novendor.JDBC4ValidConnectionChecker;
+import org.jboss.jca.adapters.jdbc.spi.ValidConnectionChecker;
+import org.jboss.logging.Logger;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
- * Implements a valid connection checker for Oracle
+ * Implements a valid connection checker for Oracle.
+ * This is a legacy implementation which uses a pingDatabase method.
  *
  * @author <a href="mailto:abrock@redhat.com">Adrian Brock</a>
  * @author <a href="mailto:jesper.pedersen@ironjacamar.org">Jesper Pedersen</a>
  */
-public class OracleValidConnectionChecker extends JDBC4ValidConnectionChecker
+public class OracleLegacyValidConnectionChecker implements ValidConnectionChecker, Serializable
 {
    private static final long serialVersionUID = 1937054230333286884L;
+
+   private static Logger log = Logger.getLogger(OracleLegacyValidConnectionChecker.class);
 
    /**
     * Constructor
     */
-   public OracleValidConnectionChecker()
+   public OracleLegacyValidConnectionChecker()
    {
-      super();
    }
 
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public SQLException isValidConnection(Connection c)
+   {
+      try
+      {
+         Method ping = SecurityActions.getMethod(c.getClass(), "pingDatabase", (Class<?>[])null);
+         SecurityActions.setAccessible(ping);
+
+         Integer status = (Integer) ping.invoke(c, (Object[])null);
+
+         // Error
+         if (status == null || status.intValue() < 0)
+            return new SQLException("pingDatabase failed status=" + status);
+      }
+      catch (Exception e)
+      {
+         return new SQLException("pingDatabase failed", e);
+      }
+
+      // OK
+      return null;
+   }
 }
