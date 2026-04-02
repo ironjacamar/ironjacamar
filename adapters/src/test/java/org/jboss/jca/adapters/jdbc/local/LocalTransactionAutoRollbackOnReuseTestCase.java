a15ba5c0066ac865b617d1967883219b63fbda1e
marco sappe griot <msappegr@ibm.com>
2026-04-02 12:15:44 +0200

    [JBJCA-1535] leftover
    moved jakarta to javax package for 1.5.x
    
    Adding Copyright

--- adapters/src/test/java/org/jboss/jca/adapters/jdbc/local/LocalTransactionAutoRollbackOnReuseTestCase.java
+++ adapters/src/test/java/org/jboss/jca/adapters/jdbc/local/LocalTransactionAutoRollbackOnReuseTestCase.java
@@ -1,3 +1,25 @@
+/*
+ * IronJacamar, a Java EE Connector Architecture implementation
+ * Copyright 2026, Red Hat Inc, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package org.jboss.jca.adapters.jdbc.local;
 
 import org.jboss.jca.adapters.jdbc.local.testimpl.MockDriver;
@@ -5,9 +27,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import jakarta.resource.spi.ConnectionEvent;
-import jakarta.resource.spi.ConnectionEventListener;
-import jakarta.resource.spi.security.PasswordCredential;
+import javax.resource.spi.ConnectionEvent;
+import javax.resource.spi.ConnectionEventListener;
+import javax.resource.spi.security.PasswordCredential;
 import javax.security.auth.Subject;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
